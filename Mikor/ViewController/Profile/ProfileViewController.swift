038de0f5cfbea88f89f915fbdb236f0cfc316759
Haojun.Zhao <haojun.zhao@icloud.com>
2016-01-28 21:55:31 +0800

    update profile

--- Mikor/ViewController/Profile/ProfileViewController.swift
+++ Mikor/ViewController/Profile/ProfileViewController.swift
@@ -59,7 +59,7 @@ class ProfileViewController: UIViewController {
         
         //设置头像
         let headborderlong = self.view.bounds.width/5
-        let head = UIImageView(frame: CGRectMake(self.view.bounds.width/2 - headborderlong/2, self.view.bounds.height * 0.1, headborderlong, headborderlong))
+        let head = UIImageView(frame: CGRectMake(self.view.bounds.width/2 - headborderlong/2, self.view.bounds.height * 0.2, headborderlong, headborderlong))
         head.backgroundColor = UIColor.whiteColor()
         //缩放默认头像
         let headtemp = UIImage(named: "defaultuser_128px")
@@ -76,7 +76,7 @@ class ProfileViewController: UIViewController {
         head.layer.masksToBounds = true
         
         //设置详细部分
-        let lrdetaillback = UIImageView(frame: CGRectMake(0, self.view.bounds.height*0.2 + headborderlong, self.view.bounds.width, (self.view.bounds.height * 0.8)-headborderlong))
+        let lrdetaillback = UIImageView(frame: CGRectMake(0, self.view.bounds.height*0.3 + headborderlong, self.view.bounds.width, (self.view.bounds.height * 0.7)-headborderlong))
         lrdetaillback.backgroundColor = UIColor.whiteColor()
         lrdetaillback.userInteractionEnabled = true
         let lblshow = UILabel(frame: CGRectMake(0,lrdetaillback.bounds.height * 0.2,self.view.bounds.width,self.view.bounds.height * 0.1))
@@ -87,31 +87,14 @@ class ProfileViewController: UIViewController {
         lrdetaillback.addSubview(lblshow)
         
         //设置按钮
-        let btnRegist = UIButton(frame: CGRectMake((lrdetaillback.bounds.width - 268)/2,lrdetaillback.bounds.height * 0.4,128,40))
-        let btnlogin = UIButton(frame: CGRectMake(lrdetaillback.bounds.width/2 + 20 ,lrdetaillback.bounds.height * 0.4,128,40))
-        
-        btnlogin.layer.borderColor = UIColor.grayColor().CGColor
-        btnRegist.layer.borderColor = UIColor.grayColor().CGColor
-        btnlogin.layer.borderWidth = 0.5
-        btnRegist.layer.borderWidth = 0.5
+        let btnlogin = UIButton(frame: CGRectMake(lrdetaillback.bounds.width/2 - 128 ,lrdetaillback.bounds.height * 0.4,256,40))
+        btnlogin.backgroundColor = UIColor.orangeColor()
         btnlogin.layer.cornerRadius = 3
-        btnRegist.layer.cornerRadius = 3
-        
-        
-        btnlogin.setTitle("登录", forState: UIControlState.Normal)
-        btnlogin.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
-        btnlogin.setTitleColor(UIColor.grayColor(), forState: UIControlState.Highlighted)
-        btnlogin.addTarget(self, action: "BtnHandler", forControlEvents: UIControlEvents.TouchUpInside)
+        btnlogin.setTitle("使用新浪微博登录", forState: UIControlState.Normal)
+        btnlogin.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+                btnlogin.addTarget(self, action: "LoginClick", forControlEvents: UIControlEvents.TouchUpInside)
         btnlogin.tag = 0
-        
-        btnRegist.setTitle("注册", forState: UIControlState.Normal)
-        btnRegist.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
-        btnRegist.setTitleColor(UIColor.grayColor(), forState: UIControlState.Highlighted)
-        btnRegist.addTarget(self, action: "BtnHandler", forControlEvents: UIControlEvents.TouchUpInside)
-        btnRegist.tag = 1
         lrdetaillback.addSubview(btnlogin)
-        lrdetaillback.addSubview(btnRegist)
-        
         self.view.addSubview(background)
         self.view.addSubview(head)
         self.view.addSubview(lrdetaillback)
@@ -129,10 +112,10 @@ class ProfileViewController: UIViewController {
         print("设置")
     }
     
-    func BtnHandler()
+    func LoginClick()
     {
         //提交一个授权请求
-        var request:WBAuthorizeRequest! = WBAuthorizeRequest.request() as! WBAuthorizeRequest
+        let request:WBAuthorizeRequest! = WBAuthorizeRequest.request() as! WBAuthorizeRequest
         request.redirectURI = RedirectURL
         request.scope = "all"
         WeiboSDK.sendRequest(request)
