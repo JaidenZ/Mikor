aeb6c7ee4058f2a7eea730f340c7e8de020710f3
Haojun.Zhao <haojun.zhao@icloud.com>
2016-02-25 10:42:21 +0800

    完善自定义Cell
    加入功能图标 标题 按钮 (位置需调整)

--- Mikor/CustomController/ProfileTableViewCell.swift
+++ Mikor/CustomController/ProfileTableViewCell.swift
@@ -15,50 +15,56 @@ class ProfileTableViewCell: UITableViewCell {
 
     //懒加载用户头像
     lazy var _profileimage:UIImageView = {
-        
-        
         let imageview:UIImageView = UIImageView(frame: CGRectMake(self.bounds.width * 0.04, self.bounds.height * 0.25, self.bounds.width * 0.18, self.bounds.width * 0.18))
         imageview.layer.cornerRadius = 29
         imageview.layer.masksToBounds = true
         return imageview
     }()
-    //懒加载标题
+    //懒加载用户标题
     lazy var _profiletitle:UILabel = {
         let title:UILabel = UILabel(frame: CGRectMake(self.bounds.width * 0.27,self.bounds.height * 0.45,self.bounds.width*0.5,self.bounds.height*0.5))
         title.font = UIFont(name: "Helvetica", size: 16)
-        
         return title
     }()
+    //懒加载用户描述
     lazy var _profiledescrib:UILabel = {
         let describ:UILabel = UILabel(frame: CGRectMake(self.bounds.width * 0.27,self.bounds.height,self.bounds.width*0.5,self.bounds.height*0.5))
         describ.font = UIFont(name: "Helvetica", size: 12)
         describ.textColor = UIColor.grayColor()
         return describ
-        
     }()
-    //let _profileacces:UIImageView
-    
-    
+    //懒加载功能按钮
+    lazy var _profileacces:UIImageView = {
+        let accesview:UIImageView = UIImageView(frame: CGRectMake(self.bounds.width * 0.8,self.bounds.height * 0.45,self.bounds.width * 0.15,self.bounds.width * 0.2))
+        return accesview
+    }()
+    //懒加载功能图标
+    lazy var _profileicon:UIImageView = {
+        let iconview:UIImageView = UIImageView(frame: CGRectMake(self.bounds.width * 0.04, self.bounds.height * 0.26, self.bounds.width * 0.15, self.bounds.width * 0.15))
+        return iconview
+    }()
+    //懒加载功能标题
+    lazy var _celltitle:UILabel = {
+        let celltitle:UILabel = UILabel(frame: CGRectMake(self.bounds.width*0.27,self.bounds.height * 0.45,self.bounds.width * 0.5,self.bounds.height*0.5))
+        celltitle.font = UIFont(name: "Helvetica",size:16)
+        return celltitle
+    }()
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
         self.backgroundView = UIImageView(image: UIImage(named: "CellBorder"))
-        self.addSubview(_profileimage)
-        self.addSubview(_profiletitle)
-        self.addSubview(_profiledescrib)
-//        self.addSubview(_profileacces)
-        
+        self.addSubview(_profileimage)//用户头像
+        self.addSubview(_profiletitle)//用户标题
+        self.addSubview(_profiledescrib)//用户描述
+        self.addSubview(_profileacces)//功能按钮
+        self.addSubview(_profileicon)//功能图标
+        self.addSubview(_celltitle)//功能标题
     }
     
-    
-    
-
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
 
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
